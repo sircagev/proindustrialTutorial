13afdde4a058acbc44ece17943d6c1c389c605cd
sircagev <cf.giron04@gmail.com>
2024-05-29 20:03:34 -0500

    Finalizando tutorial

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,9 @@
 import { defineConfig } from 'astro/config';
 
+import preact from "@astrojs/preact";
+
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  site: "https://master--peppy-bubblegum-5aab13.netlify.app",
+  integrations: [preact()]
+});
